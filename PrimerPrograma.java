75e834e62217d00698c02a12f00e2d16e713c264
alexander-CD21 <alexander.guillen.v@uni.pe>
2022-02-15 20:35:21 -0500

    terminado

---
+++ PrimerPrograma.java
@@ -0,0 +1,7 @@
+public class PrimerPrograma{
+    public static void main(String[] args){
+        System.out.println("Mi nombre es Alexander");
+        System.out.println("Tengo 25 años de edad");
+        System.out.println("Mi ciudad natal es Lima,Peru");
+    }
+}
